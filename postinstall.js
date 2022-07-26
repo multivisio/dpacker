9dd51cbd931d2d0f1229f23ff21c53962f076136
Jeannette Zybon <jeannette.zybon@multivisio.de>
2022-07-26 16:47:41 +0200

    reqpath test3

--- postinstall.js
+++ postinstall.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require('path');
-const reqpath = path.join(__dirname,"..","..","..");
+const reqpath = path.join(__dirname,"../../../");
 
 if(fs.existsSync(reqpath, "webpack.env.js")){
     console.log("Ist da");
