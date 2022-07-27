854e5b7641730b79d4d3d2e693c674903a98d9af
Jeannette Zybon <jeannette.zybon@multivisio.de>
2022-07-27 10:06:44 +0200

    reqoath perfektioniert

--- postinstall.js
+++ postinstall.js
@@ -1,14 +1,14 @@
 const fs = require("fs");
 const path = require('path');
+const reqpath = path.join(__dirname,"..","..","..");
 
-
-if(fs.existsSync("webpack.env.js")){
-    fs.writeFile('test.txt', 'ist da!', function (err) {
-        if (err) throw err;
-    });
+if(fs.existsSync(reqpath, "webpack.env.js")){
+         fs.writeFile('test.txt', 'ist da!', function (err) {
+             if (err) throw err;
+         });
 }
 else{
-    fs.writeFile('test.txt', 'nicht dad!', function (err) {
-        if (err) throw err;
-    });
+        fs.writeFile(reqpath, 'test.txt', 'nicht da!', function (err) {
+            if (err) throw err;
+        });
 }
