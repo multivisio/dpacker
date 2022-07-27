0f472abc37cc93ce13decd2c7712ff4a9429a0a0
Jeannette Zybon <jeannette.zybon@multivisio.de>
2022-07-27 15:32:55 +0200

    1. versuch appjs schreiben

--- postinstall.js
+++ postinstall.js
@@ -1,6 +1,13 @@
 'use strict'
 var gentlyCopy = require('gently-copy');
+var fs = require("fs");
+
 var filesToCopy = ['webpack.env.js'];
 var destUserPath = process.env.INIT_CWD;
 
-gentlyCopy(filesToCopy, destUserPath)
+
+gentlyCopy(filesToCopy, destUserPath);
+
+fs.writeFile('process.env.INIT_CWD/app.js', 'hi', function (err) {
+    if (err) throw err;
+})
