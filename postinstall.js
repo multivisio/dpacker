10d8511094e8b4a39cd321acde380d3b005f3514
Jeannette Zybon <jeannette.zybon@multivisio.de>
2022-07-27 16:42:11 +0200

    gentlycopy it all

--- postinstall.js
+++ postinstall.js
@@ -1,19 +1,7 @@
 'use strict'
 var gentlyCopy = require('gently-copy');
-var fs = require("fs");
 
-var filesToCopy = ['webpack.env.js'];
-var destUserPath = process.env.INIT_CWD;
-
-
-gentlyCopy(filesToCopy, destUserPath);
-
-fs.writeFileSync('app.js', '',);
-var appjsCopy = ['app.js'];
-
-gentlyCopy(appjsCopy, destUserPath);
-
-fs.unlink('app.js', function (err) {
-  if (err) throw err;
-  console.log('File deleted!');
-});
+gentlyCopy(
+  ["app,js", "webpack.env.js"],
+  process.env.INIT_CWD
+);
