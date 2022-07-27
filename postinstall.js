5df9cd5f09fd521730443450e2996361bc746f84
Jeannette Zybon <jeannette.zybon@multivisio.de>
2022-07-27 12:00:15 +0200

    gentlyCopy

--- postinstall.js
+++ postinstall.js
@@ -1,6 +1,6 @@
 'use strict'
 var gentlyCopy = require('gently-copy');
-var filesToCopy = ['.webpack.env.js']
+var filesToCopy = ['.webpack.env.js'];
 var destUserPath = process.env.INIT_CWD;
 
 gentlyCopy(filesToCopy, destUserPath)
