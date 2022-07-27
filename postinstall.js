f8666df61bf4956128f2040674d195b23fe0a1dc
Jeannette Zybon <jeannette.zybon@multivisio.de>
2022-07-27 10:46:33 +0200

    kopierversuch1

--- postinstall.js
+++ postinstall.js
@@ -1,14 +1,14 @@
 const fs = require("fs");
 const path = require('path');
-const reqpath = path.join(__dirname,"..","..","..");
 
-if(fs.existsSync(reqpath, "webpack.env.js")){
+
+if(fs.existsSync("webpack.env.js")){
          fs.writeFile('test.txt', 'ist da!', function (err) {
              if (err) throw err;
          });
 }
 else{
-        fs.writeFile(reqpath, 'test.txt', 'nicht da!', function (err) {
+        fs.writeFile('test.txt', 'nicht da!', function (err) {
             if (err) throw err;
         });
 }
