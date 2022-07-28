7e008d39135146f296d825815a8e69be4027faad
Jeannette Zybon <jeannette.zybon@multivisio.de>
2022-07-28 10:19:16 +0200

    kopieren build watch

--- postinstall.js
+++ postinstall.js
@@ -1,7 +1,18 @@
 'use strict'
 var gentlyCopy = require("gently-copy");
+const fs = require("fs");
 
 gentlyCopy(
   ["app.js", "webpack.env.js"],
   process.env.INIT_CWD
 );
+
+const json = require(process.env.INIT_CWD + "/package.json");
+
+if (!json.hasOwnProperty("scripts")){
+    json.scripts = {};
+}
+json.scripts["build"] = "webpack --config configuration/webpack.dev.config.js --mode=development ";
+json.scripts["watch"] = "webpack --config configuration/webpack.dev.config.js --mode=development --watch";
+
+fs.writeFileSync(process.env.INIT_CWD + "/package.json", JSON.stringify(json, null, 2));
